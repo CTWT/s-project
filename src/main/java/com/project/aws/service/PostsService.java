9f89d7a1264b3a18b6a1e359ebffc5d1ccc62630
setia <inchul9603@naver.com>
2022-10-26 22:42:05 +0900

    upload

---
+++ src/main/java/com/project/aws/service/PostsService.java
@@ -0,0 +1,55 @@
+package com.project.aws.service;
+
+import com.project.aws.domain.posts.Posts;
+import com.project.aws.domain.posts.PostsRepository;
+import com.project.aws.web.dto.PostsListResponseDto;
+import com.project.aws.web.dto.PostsResponseDto;
+import com.project.aws.web.dto.PostsSaveRequestDto;
+import com.project.aws.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+            return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글 없음="+ id));
+
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글 없음="+ id));
+
+        postsRepository.delete(posts);
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글 없음="+ id));
+
+        return new PostsResponseDto(entity);
+    }
+}
